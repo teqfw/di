cd2e0c3c12d78863d9bbc1d6ab9ddfcb02e4019a
Alex Gusev <alex@flancer64.com>
2019-08-22 13:34:08 +0300

    WF-195 Habr: JS DI for front & back

--- example/App.mjs
+++ example/App.mjs
@@ -5,13 +5,12 @@ export default class Vendor_Module_App {
         /** @type {Vendor_Module_Service} */
         const _service = spec.Vendor_Module_Service;
         this.name = "Vendor_Module_App";
-        console.log(`'${this.name}' instance is created (deps: [${_config.name}, ${_service.name}]).`);
 
         /**
          * @memberOf Vendor_Module_App.prototype
          */
         this.run = function () {
-            console.log(`Application '${_config.name}' is running.`);
+            console.log(`Application '${this.name}' is running (deps: [${_config.name}, ${_service.name}]).`);
         }
     }
 }
