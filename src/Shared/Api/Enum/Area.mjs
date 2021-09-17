22981e880cf00636067e85a1c64bb7621193562a
Alex Gusev <alex@flancer64.com>
2021-09-17 13:02:17 +0300

    WF-491 DTO should clone init data

---
+++ src/Shared/Api/Enum/Area.mjs
@@ -0,0 +1,10 @@
+/**
+ * Enumeration for application areas.
+ */
+const TeqFw_Di_Shared_Api_Enum_Area = {
+    BACK: 'back', // nodejs compatible 'import' is allowed
+    FRONT: 'front', // browser compatible 'import' is allowed
+    SHARED: 'shared', // relative 'import' only is allowed
+}
+Object.freeze(TeqFw_Di_Shared_Api_Enum_Area);
+export default TeqFw_Di_Shared_Api_Enum_Area;
