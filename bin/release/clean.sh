d79b05f211a23557bb8c2ea934fbd9db723c5574
Alex Gusev <alex@flancer64.com>
2021-12-20 11:11:52 +0200

    WF-536 Rollout new versions for TeqFW plugins

--- bin/release/clean.sh
+++ bin/release/clean.sh
@@ -4,6 +4,7 @@
 ##
 DIR_ROOT=${DIR_ROOT:-$(cd "$(dirname "$0")/../../" && pwd)}
 
+rm -fr "${DIR_ROOT}/doc/"
 rm -fr "${DIR_ROOT}/node_modules/"
 rm -fr "${DIR_ROOT}/package-lock.json"
 rm -fr "${DIR_ROOT}/test/"
