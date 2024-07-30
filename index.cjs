f8e880d9280f8542ac4c17b1533fa2b70a4af31a
Alex Gusev <alex@flancer64.com>
2024-07-30 16:08:29 +0300

    WF-717 Try the rollup

--- index.cjs
+++
@@ -1,5 +0,0 @@
-/**
- * Entry point to build web bundle (Common JS).
- */
-const {default: Container} = require('./src/Container.js');
-window.TeqFwDi = Container;
--- index.mjs
+++
@@ -1,6 +0,0 @@
-/**
- * Entry point to build web bundle (ES6 modules).
- */
-import Container from './src/Container.js';
-
-window.TeqFwDi = Container;
