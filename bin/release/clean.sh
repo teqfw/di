7c43d01d13370b2c709ba54fecabbd969165bc21
Alex Gusev <alex@flancer64.com>
2024-07-31 15:11:47 +0300

    WF-704 Improve the depIds in the DI plugin

--- bin/release/clean.sh
+++ bin/release/clean.sh
@@ -6,6 +6,7 @@ DIR_ROOT=${DIR_ROOT:-$(cd "$(dirname "$0")/../../" && pwd)}
 
 rm -fr "${DIR_ROOT}/demo/"
 rm -fr "${DIR_ROOT}/doc/"
+rm -fr "${DIR_ROOT}/docs/"
 rm -fr "${DIR_ROOT}/example/"
 rm -fr "${DIR_ROOT}/node_modules/"
 rm -fr "${DIR_ROOT}/test/"
