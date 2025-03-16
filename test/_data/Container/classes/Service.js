61c5e2da059e5c295c4e70e584b2887294b84dbe
Alex Gusev <alex@flancer64.com>
2025-03-16 20:12:29 +0200

    WF-773 Add an ability to put objects into container

--- test/_data/Container/classes/Service.js
+++ test/_data/Container/classes/Service.js
@@ -3,10 +3,10 @@ export default class App_Service {
         {
             App_Logger$: logger,
             App_Config: config,
-            'node:http2': nodeHttp2,
+            'node:http2': http2,
         }
     ) {
-        const {constants: {HTTP2_HEADER_CONTENT_TYPE}} = nodeHttp2;
+        const {constants: {HTTP2_HEADER_CONTENT_TYPE}} = http2;
         return function (opts) {
             logger.info(`Service '${config.appName}' is running with: ${JSON.stringify(opts)}`);
             logger.info(`Header: ${HTTP2_HEADER_CONTENT_TYPE}`);
