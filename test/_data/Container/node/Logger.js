e091752be7f7a5d9ded8fb52196b5d1a42f98197
Alex Gusev <alex@flancer64.com>
2025-03-18 11:31:07 +0200

    WF-774 Publish DI with node prefix

--- test/_data/Container/node/Logger.js
+++
@@ -1,7 +0,0 @@
-export default async function ({App_Config: config}) {
-    console.info(`Logger is created with config: '${JSON.stringify(config)}'`);
-    return {
-        error: (msg) => console.error(msg),
-        info: (msg) => console.info(msg),
-    };
-};
